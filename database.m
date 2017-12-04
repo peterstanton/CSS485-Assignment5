64f7a9fe673fd3f28ae6c6698045f168dc04710f
Peter Stanton <peterstanton@users.noreply.github.com>
2017-12-04 13:59:15 -0800

    Add files via upload
    Branch off working stuff.

--- database.m
+++ database.m
@@ -1,20 +1,39 @@
+clear
+
 % Change the filenames if you've saved the files under different names
 % On some platforms, the files might be saved as 
 % train-images.idx3-ubyte / train-labels.idx1-ubyte
 Input = loadMNISTImages('train-images.idx3-ubyte'); %inputs
 Output = loadMNISTLabels('train-labels.idx1-ubyte'); %outputs
+
+t0 = [1 0 0 0 0 0 0 0 0 0];
+t1 = [0 1 0 0 0 0 0 0 0 0];
+t2 = [0 0 1 0 0 0 0 0 0 0];
+t3 = [0 0 0 1 0 0 0 0 0 0];
+t4 = [0 0 0 0 1 0 0 0 0 0];
+t5 = [0 0 0 0 0 1 0 0 0 0];
+t6 = [0 0 0 0 0 0 1 0 0 0];
+t7 = [0 0 0 0 0 0 0 1 0 0];
+t8 = [0 0 0 0 0 0 0 0 1 0];
+t9 = [0 0 0 0 0 0 0 0 0 1];
  
+answers = [t0; t1; t2; t3; t4; t5; t6; t7; t8; t9;];
 % We are using display_network from the autoencoder code
 % display_network(images(:,1:100)); % Show the first 100 images
- LearningRate = 0.05;
+ LearningRate = 0.001;
   
-NumHidLayerNeurons = 500;
-NumOutLayer = 4;
+NumHidLayerNeurons = 70;
+NumOutLayer = 10;
+
+% r = (b-a).*rand(1000,1) + a;
+
  
 HiddenLayerWeights = rand(NumHidLayerNeurons,784); % Weight matrix from Input to Hidden
 OutputLayerWeights = rand(NumOutLayer,NumHidLayerNeurons); % Weight matrix from Hidden to Output
 biasHidden = rand(NumHidLayerNeurons,1);         % Random bias.
 biasOutput = rand(NumOutLayer,1);
+
+
 
 tarIterations = 10000;
 IterationCount = 0;  %Count passes.
@@ -22,19 +41,29 @@ ErrorVec1(1:tarIterations) = 0;
 xVec(1:tarIterations) = 0;
 i = 0;
 while(IterationCount < tarIterations)
-    if i == 6000
-        i = 1;
-    else
-        i = i + 1;
-    end
+%     if i == 6000
+%         i = 1;
+%     else
+%         i = i + 1;
+%     end
+    i = floor((rand(1)*6001) + 1);
     IterationCount = IterationCount + 1           %Increment the counter
     outOfHidden = softmax(HiddenLayerWeights * Input(:,i) + biasHidden);   
     outOfOutput = logsig(OutputLayerWeights * outOfHidden + biasOutput);
     
-   myError = decimalToBinaryVector(Output(i),NumOutLayer)' - outOfOutput;
+    targetNumber = Output(i,:);
+    targetAnswer = answers(targetNumber+1,:)';
+    
+  % e = t(:,(num+1)) - outputResult;
+   
+   myError = targetAnswer - outOfOutput;
+   
+%    s2 = -2.*diag((ones(size(outputResult))-outputResult).*outputResult)*e;
+%    s1 = diag((ones(size(hiddenResult))-hiddenResult).*hiddenResult)*outputWeights'*s2;
+
   
-   S2 = -2.*diag(ones(size(outOfOutput))-outOfOutput.*outOfOutput)*myError;   
-   S1 = diag(ones(size(outOfHidden))-outOfHidden.*outOfHidden)*OutputLayerWeights'*S2;
+   S2 = -2.*diag((ones(size(outOfOutput))-outOfOutput).*outOfOutput)*myError;   
+   S1 = diag((ones(size(outOfHidden))-outOfHidden).*outOfHidden)*OutputLayerWeights'*S2;
          
    OutputLayerWeights = OutputLayerWeights - LearningRate * S2 * outOfHidden';  
    HiddenLayerWeights = HiddenLayerWeights - LearningRate * S1 * Input(:,i)';

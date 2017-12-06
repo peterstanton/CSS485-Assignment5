6304fcf5f8070228258b8aeaecff568359b05a07
Peter Stanton <peterstanton@users.noreply.github.com>
2017-12-05 18:18:31 -0800

    Add files via upload

--- database.m
+++ database.m
@@ -36,46 +36,64 @@ biasOutput = (b-a).*rand(NumOutLayer,1) + a;
 
 tarIterations = 10000;
 
+Epochs = 10;
+
+ErrorVecMaster(Epochs:tarIterations) = 0;
 
 IterationCount = 0;  %Count passes.
-ErrorVec1(1:tarIterations) = 0;
-xVec(1:tarIterations) = 0;
+% ErrorVec1(1:tarIterations) = 0;
+ xVec(1:tarIterations) = 0;
 i = 0;
-while(IterationCount < tarIterations)
-    i = floor((rand(1)*6001) + 1);
-%     if i == 60000
-%         i = 1;
-%     else 
-%         i = i + 1;
-%     end
-    IterationCount = IterationCount + 1;         %Increment the counter
-    outOfHidden = tanh(HiddenLayerWeights * Input(:,i) + biasHidden);   
-    outOfOutput = logsig(OutputLayerWeights * outOfHidden + biasOutput);
-    
-   Label = Output(i,:);
-   tarVector = answers(:,Label+1);
-   myError = tarVector - outOfOutput;
-  
-   S2 = -2.*diag(ones(size(outOfOutput))-outOfOutput.*outOfOutput)*myError;   
-   S1 = diag(ones(size(outOfHidden))-outOfHidden.*outOfHidden)*OutputLayerWeights'*S2;
-         
-   OutputLayerWeights = OutputLayerWeights - LearningRate * S2 * outOfHidden';  
-   HiddenLayerWeights = HiddenLayerWeights - LearningRate * S1 * Input(:,i)';
-   
-   biasOutput = biasOutput - LearningRate.*S2;
-   biasHidden = biasHidden - LearningRate.*S1;
-   
-   xVec(IterationCount) = IterationCount;
-   ErrorVec1(IterationCount) = sum(myError.^2)/length(myError);
-   tester = sum(myError.^2);    
+thisEpoch = 1;
+for e = 1:Epochs
+    while(IterationCount < tarIterations)
+        i = floor((rand(1)*6001) + 1);
+        IterationCount = IterationCount + 1         %Increment the counter
+        outOfHidden = tanh(HiddenLayerWeights * Input(:,i) + biasHidden);   
+        outOfOutput = logsig(OutputLayerWeights * outOfHidden + biasOutput);
+
+       Label = Output(i,:);
+       tarVector = answers(:,Label+1);
+       myError = tarVector - outOfOutput;
+
+       S2 = -2.*diag(ones(size(outOfOutput))-outOfOutput.*outOfOutput)*myError;   
+       S1 = diag(ones(size(outOfHidden))-outOfHidden.*outOfHidden)*OutputLayerWeights'*S2;
+
+       OutputLayerWeights = OutputLayerWeights - LearningRate * S2 * outOfHidden';  
+       HiddenLayerWeights = HiddenLayerWeights - LearningRate * S1 * Input(:,i)';
+
+       biasOutput = biasOutput - LearningRate.*S2;
+       biasHidden = biasHidden - LearningRate.*S1;
+
+       xVec(IterationCount) = IterationCount;
+       % ErrorVec1(IterationCount) = sum(myError.^2)/length(myError);
+       ErrorVecMaster(thisEpoch,IterationCount) = sum(myError.^2)/length(myError);
+       tester = sum(myError.^2);    
+    end
+    HiddenLayerWeights = (b-a).*rand(NumHidLayerNeurons,784) + a; % Weight matrix from Input to Hidden
+    OutputLayerWeights = (b-a).*rand(NumOutLayer,NumHidLayerNeurons) + a; % Weight matrix from Hidden to Output
+    biasHidden = (b-a).*rand(NumHidLayerNeurons,1) + a;         % Random bias.
+    biasOutput = (b-a).*rand(NumOutLayer,1) + a;
+    IterationCount = 0;
+    thisEpoch = thisEpoch + 1
+    LearningRate = LearningRate * 2;
 end
+    
 
-figure(1)
-plot(xVec,ErrorVec1)
-title('Backpropagation Network Training')
-xlabel('Backpropagation Iterations')
-ylabel('Squared Error')
+% figure(1)
+% plot(xVec,ErrorVec1)
+% title('Backpropagation Network Training')
+% xlabel('Backpropagation Iterations')
+% ylabel('Squared Error')
 
+for a = 1:Epochs
+    figure(a)
+    plot(xVec,ErrorVecMaster(a,:))
+    title('Backpropagation Network Training for Variable Learning')
+    xlabel('Backpropagation Iterations')
+    ylabel('Squared Error')
+end
+    
 
 function res = mySoftMax(n)
     res = exp(n)/sum(exp(n));
